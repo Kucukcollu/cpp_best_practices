741113fcc57ed11c6cea5b1f48059848234a67c8
Ali Aydin Kucukcollu <kucukcollu@outlook.com>
2024-08-31 16:27:41 +0300

    chapter 7 : sanitizers

---
+++ chapter_07/README.md
@@ -0,0 +1,9 @@
+# 7. Use the Tools: Sanitizers
+
+## setup
+```bash
+mkdir build
+cd build
+cmake ..
+make
+```
---
+++ chapter_07/src/main.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    int number = 5;
+    std:: cout << number / 0 << std::endl;
+
+    return 0;
+}
